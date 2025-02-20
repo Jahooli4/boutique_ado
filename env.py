f47c58730323181b26f088c22a087e99b66b62f9
Michelle <michelle.jones.2161@hotmail.co.uk>
2025-02-20 12:38:16 +0000

    add order summary to checkout success page

--- env.py
+++ env.py
@@ -1,3 +1,6 @@
 import os
 
-os.environ.setdefault('SECRET_KEY', 'django-insecure-%jgrcsb%4bkycs4%#8gsnn=%3hub0ok6go7i_^mhx1r#!!%6@_')
+os.environ.setdefault('STRIPE_PUBLIC_KEY', 'pk_test_51QuBFGJo5QlAEZoryHZryMoHoaYW5I2JXoHgMMncUo7i3c4wrXh3dXVxpO86uw3utHIlGcUGtun82pN9sGopAqXm00iFCATW6N') # noqa
+os.environ.setdefault('STRIPE_SECRET_KEY', 'sk_test_51QuBFGJo5QlAEZorcPJcnDAoRqBInjIQCn04S3VuexFDuBiwruOHx76pf7brOq8mCd8Zerl8V4ilXDB3TV7TiTPy00oCu8rLZ7') # noqa
+
+os.environ.setdefault('SECRET_KEY', 'django-insecure-%jgrcsb%4bkycs4%#8gsnn=%3hub0ok6go7i_^mhx1r#!!%6@_') # noqa
