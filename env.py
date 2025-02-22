6ac350dd8aeaa2273d8698a86573fdee7303cbd3
Michelle <michelle.jones.2161@hotmail.co.uk>
2025-02-22 14:44:18 +0000

    test webhooks

--- env.py
+++ env.py
@@ -4,3 +4,5 @@ os.environ.setdefault('STRIPE_PUBLIC_KEY', 'pk_test_51QuBFGJo5QlAEZoryHZryMoHoaY
 os.environ.setdefault('STRIPE_SECRET_KEY', 'sk_test_51QuBFGJo5QlAEZorcPJcnDAoRqBInjIQCn04S3VuexFDuBiwruOHx76pf7brOq8mCd8Zerl8V4ilXDB3TV7TiTPy00oCu8rLZ7') # noqa
 
 os.environ.setdefault('SECRET_KEY', 'django-insecure-%jgrcsb%4bkycs4%#8gsnn=%3hub0ok6go7i_^mhx1r#!!%6@_') # noqa
+
+os.environ.setdefault('STRIPE_WH_SECRET', 'whsec_9bc9d8dd3254b2849a7451a58c5e0d8cc6b561ec15416938d63293298f0afe6c') # noqa
